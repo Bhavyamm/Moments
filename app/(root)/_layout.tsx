183fa9a5b989e7d8ba1d2be23de75a7304210416
Bhavyam Mittal <bhavyamm01@gmail.com>
2025-02-17 22:55:05 +0530

    Implement camera functionality with permissions and video recording support

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,18 +1,5 @@
-import { View, Text } from "react-native";
 import { Redirect, Slot } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-
-const Header = () => {
-  return (
-    <SafeAreaView edges={["top"]} className="bg-orange-100 p-2">
-      <View className="h-10 bg-orange-100 justify-end items-center pb-2">
-        <Text className="text-center font-roboto text-primary-100 font-bold text-3xl">
-          Moments
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-};
+import { Header } from "@/components/Header";
 
 export default function AppLayout() {
   let isLogged = true;
@@ -21,5 +8,10 @@ export default function AppLayout() {
     return <Redirect href="/welcome" />;
   }
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Slot />
+    </>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export const Header = () => {
+  return (
+    <SafeAreaView edges={["top"]} className="bg-orange-100 p-2">
+      <View className="h-10 bg-orange-100 justify-end items-center pb-2">
+        <Text className="text-center font-roboto text-primary-100 font-bold text-3xl">
+          Moments
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
