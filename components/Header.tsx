a83f7c521a45fdd77d2b5b783424de8aee6c3e0b
Bhavyam Mittal <bhavyamm01@gmail.com>
2025-02-28 20:30:16 +0530

    Added back button in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { useGlobalContext } from "@/lib/global-provider";
 import { useRouter } from "expo-router";
 import { Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
 export const Header = () => {
   const { user } = useGlobalContext();
@@ -11,12 +12,23 @@ export const Header = () => {
     router.push("/profile");
   };
 
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back();
+    }
+  };
+
   return (
     <SafeAreaView edges={["top"]} className="bg-orange-100 p-2">
       <View className="relative flex h-10 justify-center items-center pb-2">
+        <TouchableOpacity onPress={handleBack} className="absolute left-2">
+          <Ionicons name="arrow-back" size={24} color="#1F2937" />
+        </TouchableOpacity>
+
         <Text className="text-center font-montserrat-bold text-primary-100 text-3xl">
           Memories
         </Text>
+
         <TouchableOpacity
           onPress={handleAvatarPress}
           className="absolute right-2"
